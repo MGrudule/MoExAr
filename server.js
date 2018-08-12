a38a4772a031f9714704a1671b37de9bfa99ec46
MGrudule <hello@maijagrudule.com>
2018-08-12 16:15:05 +0200

    johnny-five blink

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var express = require( 'express' ),
   defaults = {
     baudRate: 9600
   },
+  Board = require( "firmata" ),
   ///for correct display of data from Serial port
   Readline = SerialPort.parsers.Readline;
 
@@ -30,7 +31,25 @@ app.get( '/', function ( req, res ) {
 
 } );
 
+// Dependancy upon Johnny-Five
+var five = require( "johnny-five" );
 
+// Set up references to Arduino Board and LED
+var myBoard, myLed;
+
+// Initialise Microcontroller
+myBoard = new five.Board();
+
+// After successful initialisation of the board this code block will be run
+myBoard.on( "ready", function () {
+
+  // Instantiate a LED Object (Arduino Uno has a LED attached to Pin 13)
+  myLed = new five.Led( 13 );
+
+  // Strobe the LED (ms)
+  myLed.strobe( 300 );
+
+} );
 
 // Log that the servers running
 console.log( "Server running on port: " + port );
@@ -40,10 +59,14 @@ io.sockets.on( 'connection', function ( socket ) {
   console.log( "socket connetion innitiated" );
 
   // ///ARDUINO SETUP
-  // portArduino = new SerialPort("/dev/cu.usbmodem14111", defaults);
-  // //const portArduino = new SerialPort('/dev/tty-usbserial1');
+  //portArduino = new SerialPort("/dev/cu.usbmodem14111", defaults);
+  //const portArduino = new SerialPort('/dev/tty-usbserial1');
   // var parser = new Readline();
-
+  // var board = new Board( new SerialPort( "/dev/cu.usbmodem14121", defaults ) );
+  // board.on( "ready", () => {
+  //   // Arduino is ready to communicate
+  //   console.log( "Arduino is ready" );
+  // } );
   // When a browser connects, store its socket id
   socket.on( 'browser_connect', function () {
 
